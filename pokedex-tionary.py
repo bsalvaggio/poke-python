6503509142db2604675922dbcecf688ffb8e797a
Bill Salvaggio <119374432+bsalvaggio@users.noreply.github.com>
2023-02-07 16:39:26 +0000

    fixed shebang and a couple typos preventing it from running?

--- pokedex-tionary.py
+++ pokedex-tionary.py
@@ -1,13 +1,15 @@
-# objective: build a dictionary that allows users to search for information about specific Pokémon such as their type, abilities, and stats
+#!/usr/bin/env python
 
+# objective: build a dictionary that allows users to search for information about specific Pokémon such as their type, abilities, and stats
 
 #create a dictionary to store data on each Pokemon 
 pokemon_info = {
     "Bulbasaur": {"type": "Grass", "abilities": ["Overgrow", "chlorophyll"], "stats": [45, 49, 49, 65, 65, 45]},
     "Ivysaur": {"type": "Grass", "abilities": ["Overgrow", "Chlorophyll"], "stats": [60, 62, 63, 80, 80, 60]},
-    "Venusaur": {"type": "Grass", "abilities": ["Overgrow", "Chlorophyll"]  "stats": [100, 100, 100, 100, 100, 100]},
+    "Venusaur": {"type": "Grass", "abilities": ["Overgrow", "Chlorophyll"],  "stats": [100, 100, 100, 100, 100, 100]},
 }
 ...
+
 #Create a function- 
 #get_pokemon_info(pokemon_name) that takes and returns the information about that Pokémon
 
